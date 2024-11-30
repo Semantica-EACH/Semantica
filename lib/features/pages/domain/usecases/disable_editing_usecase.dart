59cbfe77b4b883493601d09345688cc2adf435fe
Márcio Moretto Ribeiro <marcio.mr@gmail.com.br>
2024-11-30 00:59:31 -0300

    Tudo funcionando e usando a arquitetura limpa. Só um dos testes não consegui fazer.

--- lib/features/pages/domain/usecases/disable_editing_usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:semantica/features/pages/domain/entities/page.dart';
-
-class DisableEditingUseCase {
-  void call(Page page) {
-    page.isEditing = false;
-  }
-}
--- lib/features/pages/domain/usecases/enable_editing_usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:semantica/features/pages/domain/entities/page.dart';
-
-class EnableEditingUseCase {
-  void call(Page page) {
-    page.isEditing = true;
-  }
-}
